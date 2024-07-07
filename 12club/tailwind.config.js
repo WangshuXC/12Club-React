25d0a02c6bdc6ae6247fe816770bf3bae9cbbdb9
apexliang <apexliang@tencent.com>
2024-07-08 00:00:04 +0800

    feat: 增加latestUpdate组件响应式匹配

--- 12club/tailwind.config.js
+++ 12club/tailwind.config.js
@@ -3,7 +3,15 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: 'selector',
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '1k': '1700px',
+      '2k': '2000px',
+    }
   },
   plugins: [],
 }
